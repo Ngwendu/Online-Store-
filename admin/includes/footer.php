5b4e564e9f7fdad9a97fcc32eeabf6fbc29848a4
Ngwendu <gambungwendu@gmail.com>
2022-12-15 11:17:54 +0200

    updated

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,5 +2,5 @@
     <div class="pull-right hidden-xs">
       <b>All rights reserved</b>
     </div>
-    <strong>Copyright &copy; 2018 Brought To You By<a href="https://code-projects.org/">Code-Projects</a></strong>
+    <strong>Copyright &copy; 2022 Brought To You By<a href="https://Maneno.co.za/"><Maneno-Technologies></Maneno-Technologies></a></strong>
 </footer>
